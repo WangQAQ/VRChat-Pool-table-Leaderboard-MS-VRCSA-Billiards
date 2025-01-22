b4f3101b1330fa13c7fce9c9224a4d5d9d936a75
WangQAQ123 <89381371+WangQAQ@users.noreply.github.com>
2025-01-23 06:59:17 +0800

    计分器更新
    优化同步，测试版新计分器

---
+++ Plug/WangQAQ/Score V4/C#/Sc V4/interface/IScoreAPI.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class IScoreAPI : UdonSharpBehaviour
+{
+	#region API
+
+	/* 初始化 */
+	public virtual void _Init(BilliardsModule billiardsModule) { /* Nop */  }
+
+	/* Tick函数 */
+	public virtual void _Tick() { /* Nop */ }
+
+	//API lobbyPlayerList
+	public virtual void _LobbyOpen(string[] lobbyPlayerList) { /* Nop */  }
+
+	//API nowPlayerList
+	public virtual void _PlayerChanged(string[] nowPlayerList, int gameState) { /* Nop */  }
+
+	//API startPlayerList
+	public virtual void _GameStarted(string[] startPlayerList) { /* Nop */  }
+
+	//API winningTeamLocal
+	public virtual void _GameEnd(uint winningTeamLocal) { /* Nop */  }
+
+	/* 刷新计分器 */
+	public virtual void _GameReset() { /* Nop */  }
+
+	#endregion
+}
